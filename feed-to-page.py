84755a369a5c7a2ee9485f0d2922a78b351ca342
Julian Hyde <jhyde@apache.org>
2018-12-04 11:18:47 -0800

    Add 2018 to the archive

---
+++ feed-to-page.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+#
+# Reads the XML feed and generates an HTML web page
+#
+import xml.etree.ElementTree as ET
+import re
+
+tree = ET.parse('web/archive/2018/feed.xml')
+root = tree.getroot()
+ns = "{http://www.w3.org/2005/Atom}"
+print("<html>")
+print("<head><title>Daily Hawk Count (2018)</title></head>")
+print("<body>")
+for entry in root.iter(ns + "entry"):
+  # "Sunday, December 02, 2018"
+  title = entry.find(ns + "title").text
+  # hydromatic-ggro-20181202
+  id = entry.find(ns + "id").text
+  tag = re.sub(r".*-", "", id)
+  print("<h2><a name='" + tag + "'/>" + title + "</h2>")
+  content = entry.find(ns + "content").text
+  print("<p>" + content + "</p>").encode('utf-8')
+  print
+print("</body>")
+print("</html>")
+
+# End feed-to-page.py
