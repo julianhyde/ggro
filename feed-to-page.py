1a283f3d61041051004003a02a953dc16b55da63
Julian Hyde <jhyde@apache.org>
2019-12-01 14:10:47 -0800

    Add 2019 to the archive

--- feed-to-page.py
+++ feed-to-page.py
@@ -2,14 +2,19 @@
 #
 # Reads the XML feed and generates an HTML web page
 #
+# Usage:
+#   python ./feed-to-page.py > web/archive/2018/page.html
+#
+# (replacing 2018 with the current year)
+#
 import xml.etree.ElementTree as ET
 import re
 
-tree = ET.parse('web/archive/2018/feed.xml')
+tree = ET.parse('web/archive/2019/feed.xml')
 root = tree.getroot()
 ns = "{http://www.w3.org/2005/Atom}"
 print("<html>")
-print("<head><title>Daily Hawk Count (2018)</title></head>")
+print("<head><title>Daily Hawk Count (2019)</title></head>")
 print("<body>")
 for entry in root.iter(ns + "entry"):
   # "Sunday, December 02, 2018"
